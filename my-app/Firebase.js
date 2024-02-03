8b50bed2a418749e5c7b05cfd35b4988790250dd
akshat <akshatbhatya@gmail.com>
2024-02-03 10:41:29 +0530

    install firebase and add api to env file

---
+++ my-app/Firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.React_Api,
+  authDomain: "manage-accounts-c907b.firebaseapp.com",
+  projectId: "manage-accounts-c907b",
+  storageBucket: "manage-accounts-c907b.appspot.com",
+  messagingSenderId: "37678589450",
+  appId: "1:37678589450:web:0963d2c66fd7fd2c2861f3",
+  measurementId: "G-3W3VZ4B4GN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
